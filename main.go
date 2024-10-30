88aba4631b3afac65f4e5edd845401e112fdce2c
roxxxiey <agd8@tpu.ru>
2024-10-30 10:24:00 +0700

    modified packege, add some function

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package syslogwriter
 import (
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 )
 
 // SyslogWriter - структура для записи логов в файл и отправки на сервер через UDP
@@ -31,21 +33,64 @@ func NewSyslogWriter(address string, logFile *os.File) (*SyslogWriter, error) {
 	return &SyslogWriter{conn: conn, LogFilePath: logFile}, nil
 }
 
+func (s *SyslogWriter) Emergency(input string) {
+	s.Write("<8>", input)
+}
+
+func (s *SyslogWriter) Alert(input string) {
+	s.Write("<9>", input)
+}
+
+func (s *SyslogWriter) Critical(input string) {
+	s.Write("<10>", input)
+}
+
+func (s *SyslogWriter) Error(input string) {
+	s.Write("<11>", input)
+}
+
+func (s *SyslogWriter) Warning(input string) {
+	s.Write("<12>", input)
+}
+
+func (s *SyslogWriter) Notice(input string) {
+	s.Write("<13>", input)
+}
+
+func (s *SyslogWriter) Info(input string) {
+	s.Write("<14>", input)
+}
+
+func (s *SyslogWriter) Debug(input string) {
+	s.Write("<15>", input)
+}
+
 // Write - пишет лог в файл и отправляет его на сервер
-func (w *SyslogWriter) Write(p string) error {
-	message := p
+func (s *SyslogWriter) Write(er, input string) error {
+
+	host, err := os.Hostname()
+	if err != nil {
+		host = "unknown"
+	}
+	executable, err := os.Executable()
+	if err != nil {
+		executable = "unknown"
+	}
+	processName := filepath.Base(executable)
+
+	message := er + Time() + " " + host + " " + processName + ": " + input
 	if !strings.HasSuffix(message, "\n") {
 		message += "\n"
 	}
 
 	// Записываем лог в файл
-	_, err := w.LogFilePath.WriteString(p + "\n")
+	_, err = s.LogFilePath.WriteString(message)
 	if err != nil {
 		return err
 	}
 
 	// Отправляем лог на сервер
-	_, err = w.conn.Write([]byte(message))
+	_, err = s.conn.Write([]byte(message))
 	if err != nil {
 		return err
 	}
@@ -54,6 +99,10 @@ func (w *SyslogWriter) Write(p string) error {
 }
 
 // Close - закрывает соединение с сервером
-func (w *SyslogWriter) Close() error {
-	return w.conn.Close()
+func (s *SyslogWriter) Close() error {
+	return s.conn.Close()
+}
+
+func Time() string {
+	return time.Now().Format("Jan 02 15:04:05")
 }
